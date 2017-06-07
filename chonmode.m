e9e7b542e8e28fba9149f70743809c11c1b91d87
Hoai Thuat <hoaithuat09@gmail.com>
2017-06-08 03:02:56 +0700

    update file chonmode.m

--- chonmode.m
+++ chonmode.m
@@ -1,3 +1,15 @@
+plgio=input(' nhap vao cac nut la chi co turbin gio (dang mang) [...]:');
+plmt=input(' nhap vap cac nut chi co nang luong mat troi: ');
+pldc=input(' nhap vao cac nut chi co chua dong co may phat: ');
+plpin= input('nhap vao cac nut chi co chua battery: ');
+plhh=input('nhap vao nut co nhieu loai nguon');
+%plhh(i) là so thu tu cua nut co chua nguon hon hop%
+nguon=[0 0 0 0 0 0 0];
+for lap=1:lenght(plhh)
+    fprintf('nhap vao thu tu cac nguon tai nut %d ',plhh(lap));
+    gtri=input('nhap vao thu tu cong suat cac loai may phat [Pgio Pmt Ppin Pdc Qdc]');
+    nguon(lap,:)=gtri;
+end
 list=[' please chose one mode you want to change:         '
       '    mode                                 select    '
       'mode 1: Change Wind                       1        '
@@ -19,12 +31,42 @@ list=[' please chose one mode you want to change:         '
   end;
   fprintf('\n');
   switch select
-      case 1
-          
-      case 2
-      case 3
+      case 1 % thay doi turbin gio%
+          for cs1=1:lenght(plgio)
+          busdata(plgio(cs1),7) =0;
+          busdata(plgio(cs1),8) =0;
+          busdata(plgio(cs1),9) =0;
+          busdata(plgio(cs1),10)=0;
+          end
+          %xu li cac nut nguon hon hop%
+          for cs11=1:lenght(plhh)
+              busdata(plhh(cs11),7)=busdata(plhh(cs11),7)-nguon(cs11,1);
+          end
+      case 2 %thay doi nang luong mat troi%
+          for cs1=1:lenght(plmt)
+          busdata(plmt(cs1),7) =0;
+          busdata(plmt(cs1),8) =0;
+          busdata(plmt(cs1),9) =0;
+          busdata(plmt(cs1),10)=0;
+          end
+          %xu li cac nut nguon hon hop%
+          for cs11=1:lenght(plhh)
+              busdata(plhh(cs11),7)=busdata(plhh(cs11),7)-nguon(cs11,2);
+          end          
+      case 3 %thay doi pin%
+          for cs1=1:lenght(plmt)
+          busdata(plmt(cs1),7) =0;
+          busdata(plmt(cs1),8) =0;
+          busdata(plmt(cs1),9) =0;
+          busdata(plmt(cs1),10)=0;
+          end
+          %xu li cac nut nguon hon hop%
+          for cs11=1:lenght(plhh)
+              busdata(plhh(cs11),7)=busdata(plhh(cs11),7)-nguon(cs11,2);
+          end                    
       case 4
-          
+      case 5
+      case 6
   end
   cont=input('Do you want to continue? [Y/n]');
  end
